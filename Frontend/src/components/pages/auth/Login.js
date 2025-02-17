b8a6999024f71687e8eb0eb103b300761ee6f0ca
Amancodes26 <amansngh8586@gmail.com>
2025-02-18 01:08:51 +0530

    add axios import to Login component for API integration

--- Frontend/src/components/pages/auth/Login.js
+++ Frontend/src/components/pages/auth/Login.js
@@ -14,6 +14,7 @@ import { styled } from "@mui/system";
 import { Link, useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import "./auth.css";
+
 import axios from "axios";
 import { UserContext } from "../../../context/UserContext";
 
